fc74e82e927c1301761be7d9426e17dd2936fb87
Liudmyla <kosmosjatko@gmail.com>
2024-03-26 23:27:24 +0200

    lesson1 next

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,15 @@
+import {FC, PropsWithChildren} from 'react';
+
+interface IProps extends PropsWithChildren {
+
+}
+
+const PostsPage: FC<IProps> = () => {
+    return (
+        <div>
+            PostsPage
+        </div>
+    );
+};
+
+export default PostsPage;
---
+++ src/app/users/page.tsx
@@ -0,0 +1,15 @@
+import {FC, PropsWithChildren} from 'react';
+
+interface IProps extends PropsWithChildren {
+
+}
+
+const UsersPage: FC<IProps> = () => {
+    return (
+        <div>
+            UsersPage
+        </div>
+    );
+};
+
+export default UsersPage;
